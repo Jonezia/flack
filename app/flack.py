34e37eebc10f7f28223fa1733bb1b4ecb9919749
Jonezia <adamj6636@gmail.com>
2018-08-17 14:04:24 +0800

    Initial commit for register page

--- app/flack.py
+++ app/flack.py
@@ -1,16 +1,35 @@
 import os
 
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, jsonify, request, url_for, redirect
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+users = {}
 
-@app.route("/login")
+@app.route("/", methods=['GET','POST'])
+def index():
+    if request.method == 'GET':
+        if session.get('username') is None or session['username'] == "":
+            loggedin = False
+            return redirect(url_for('login'))
+        else:
+            loggedin = True
+            return render_template("index.html")
+#logging in from login.html to index.html
+    elif request.method == 'POST':
+        if request.form['password'] == users[request.form['username']]:
+            session['username'] = request.form['username']
+            return redirect(url_for('index'))
+        else:
+            return redirect(url_for('login'))
+
+@app.route("/login", methods=['GET','POST'])
 def login():
     return render_template("login.html")
+
+@app.route("/register")
+def register():
+    return render_template('register.html')
