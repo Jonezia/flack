f2cf1ab7de521448a2a65e8447a1d2463f38a3d7
Jonezia <adamj6636@gmail.com>
2018-08-18 16:14:36 +0800

    Login functionality, merged login and register css files

--- app/flack.py
+++ app/flack.py
@@ -1,13 +1,14 @@
 import os
 
-from flask import Flask, render_template, jsonify, request, url_for, redirect
+from flask import Flask, render_template, jsonify, request, url_for, redirect, session, flash
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
+app.secret_key = 'adamiscool'
 
-users = {}
+users = {"admin":"adminpass"}
 
 @app.route("/", methods=['GET','POST'])
 def index():
@@ -18,17 +19,26 @@ def index():
         else:
             loggedin = True
             return render_template("index.html")
-#logging in from login.html to index.html
     elif request.method == 'POST':
         if request.form['password'] == users[request.form['username']]:
             session['username'] = request.form['username']
             return redirect(url_for('index'))
         else:
+            flash("Login failed")
             return redirect(url_for('login'))
 
 @app.route("/login", methods=['GET','POST'])
 def login():
-    return render_template("login.html")
+    if request.method == 'GET':
+        return render_template("login.html")
+    elif request.method == 'POST':
+        if request.form['username'] not in users:
+            users[request.form['username']] = request.form['password']
+            flash("Account registered, " + request.form['username'])
+            return redirect(url_for("login"))
+        else:
+            flash("Username in use")
+            return redirect(url_for("register"))
 
 @app.route("/register")
 def register():
